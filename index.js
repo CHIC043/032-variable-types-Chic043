25011b158ff79dfaed6ccdbb504e2da7e1f74e87
CHIC043 <karsen.kapayou@students.msswarriors.org>
2026-04-14 18:34:36 +0000

    chexk

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// Define a string variable named animalName
+let animalName = "Cat";
 
+// Define a numeric variable named height
+let height = 25;
+
+// Define a boolean variable named isIndoorPet
+let isIndoorPet = true;
+
+// Assign the innerHTML of the HTML element with the id result to the animalName
+document.getElementById('result').innerHTML = animalName;
